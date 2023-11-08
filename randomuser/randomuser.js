e83f143994499ce752e8473a239d2b21eb7ffdb3
EdwardArSan <edwardarmenta71@gmail.com>
2023-11-08 12:59:18 -0700

    randomuser

--- randomuser/randomuser.js
+++ randomuser/randomuser.js
@@ -1,12 +1,40 @@
+var numero
+var cantidad
+var datos = [];
+
 async function apirandomuser(){
-   const respuesta = await fetch("https://randomuser.me/api/");
-   const datos = await respuesta.json(); //espero la respuesta del servidor
-   let foto = document.getElementById("foto"); 
-   let nombre = document.getElementById("nombre");
-   let telefono = document.getElementById("telefono");
-   foto.setAttribute("src",datos.results[0].picture.large);
-   nombre.innerHTML = datos.results[0].name.first+" "+datos.results[0].name.last;
-   nombre.style.color = "red";
-   telefono.innerHTML = datos.results[0].phone;
-   
+    numero = 0;
+    cantidad = document.getElementById("cantidad").value;
+    const respuesta = await fetch("https://randomuser.me/api?results="+cantidad);
+    datos = await respuesta.json();
+    muestra();
+    console.log(datos);
+}
+
+function anterior(){
+  numero --;
+  if (numero<0){
+    numero=0;
+
+  }
+  muestra();    
+}
+
+function siguiente(){
+  numero ++;
+  if (numero=cantidad){
+      numero=0;
+  }
+    muestra();
+}
+
+function muestra(){
+  console.log("arreglo posicion"+numero);
+  let foto = document.getElementById("foto");
+  let nombre =document.getElementById("nombre");
+  let telefono =document.getElementById("telefono");
+  foto.setAttribute("src",datos.results[numero].picture.large);
+  nombre.innerHTML=datos.results[numero].name.first+" "+datos.results[numero].name.last;
+  nombre.style.color= "red";
+  telefono.innerHTML= datos.results[numero].phone;
 }
